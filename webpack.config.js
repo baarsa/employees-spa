43ef575dc954bc514ef0dc0821afb6bcb59f9f55
Barist <kharchevb@gmail.com>
2018-08-12 09:37:50 +0300

    initial react config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+process.env.NODE_ENV = 'development';
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: "/build"
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.es6'],
+  }
+};
